9df796daa207d19a86cbb0236e47d6c8259e53ca
erdemdenizli <erdemdenizli@yahoo.com>
2019-07-30 11:53:30 -0500

    third commit

--- src/StairsRevers.java
+++ src/StairsRevers.java
@@ -1,5 +1,5 @@
 
-public class StairsRevers {
+public class StairsRevers extends Object{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/Test.java
+++ src/Test.java
@@ -1,48 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Test {
-
+	
 	public static void main(String[] args) {
-
-		String str = "AAABBCCDA";
 		
-		ArrayList <String> list= new ArrayList<>(Arrays.asList(str.split("")));
-		ArrayList <Integer> list2= new ArrayList<>();
+		System.out.println(greaterThanFive(5));
 		
-		String newStr="";
-		int count=0;
-		
-		for(int i=0; i<list.size();i++) {
-			
-			for(int j=i+1; j<list.size();j++) {
-				
-				if(list.get(i).equals(list.get(j))) {
-					
-					count++;
-					
-					list.remove(j);
-					
-					j--;
-					
-				} else {
-					break;
-				}
-			}
-			newStr += list.get(i);
-			list2.add(count+1);
-			count=0;
-		}
-		
-		
-		String newNewStr = "";
-		
-		for (int i = 0; i < newStr.length(); i++) {
-			newNewStr += list2.get(i) + "" + newStr.charAt(i);
-		}
-		
-		System.out.println(newNewStr);
+		System.out.println(isEnes("Enes"));
+		System.out.println(isEnes("eNeS"));
 		
 	}
-
+	
+	public static boolean greaterThanFive(int n) {
+		
+		return n>5;
+		
+	}
+	
+	public static boolean isEnes(String name) {
+		
+		return name.equalsIgnoreCase("enes");
+		
+	}
+	
 }
